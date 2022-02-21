bcc1c463c9f0a06e2899be4657b7b009260dc10c
valpowebmaster <77160826+valpowebmaster@users.noreply.github.com>
2022-02-21 11:50:35 -0600

    adding files to repository

---
+++ script.js
@@ -0,0 +1,17 @@
+let percentButton = document.getElementsByClassName('tip-button');
+let buttonLength = percentButton.length;
+let i;
+
+function calcTip(e){
+    let billAmount = parseFloat(document.getElementById('bill-amount').value || 0);
+    let tipPercentage = e.target.value;
+    let numberPeople = document.getElementById('numpeople').value || 1;
+    let tipAmount = ((billAmount * tipPercentage) / 100) / numberPeople;
+    let totalAmount = (billAmount / numberPeople) + tipAmount;
+    document.getElementById('tip-amount').value = '$' + tipAmount;
+    document.getElementById('bill-total').value = '$' + totalAmount;
+}
+
+for (i = 0; i < buttonLength; i++){
+    percentButton[i].addEventListener('click', calcTip);
+}
