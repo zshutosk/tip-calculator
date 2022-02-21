4432e27768955d56d5d9fee524dfd9bc1aa87905
valpowebmaster <77160826+valpowebmaster@users.noreply.github.com>
2022-02-21 12:09:39 -0600

    updating equation in js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function calcTip(e){
     let billAmount = parseFloat(document.getElementById('bill-amount').value || 0);
     let tipPercentage = e.target.value;
     let numberPeople = document.getElementById('numpeople').value || 1;
-    let tipAmount = ((billAmount * tipPercentage) / 100) / numberPeople;
-    let totalAmount = (billAmount / numberPeople) + tipAmount;
+    let tipAmount = Math.round(((billAmount * tipPercentage) / 100) / numberPeople);
+    let totalAmount = Math.round((billAmount / numberPeople) + tipAmount);
     document.getElementById('tip-amount').value = '$' + tipAmount;
     document.getElementById('bill-total').value = '$' + totalAmount;
 }
